ac1b6218400fd34d3aa4e443af81fcb2db3cc715
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-02-10 19:25:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 
 const loginsec = document.querySelector('.login-section')
 const Registerlink = document.querySelector('.Register-link')
 const Loginlink = document.querySelector('.Login-link')
 
-Loginlink.addEventListener('click', ()=>{
+Loginlink.addEventListener('click', () => {
   loginsec.classList.add('active')
 })
-Registerlink.addEventListener('click', ()=>{
+Registerlink.addEventListener('click', () => {
   loginsec.classList.remove('active')
 })
